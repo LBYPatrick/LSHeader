0e4f6c765bcab8fa94fd7ad67258b108c9bdfcea
LBYPatrick <lbypatrick@gmail.com>
2017-10-03 12:58:43 -0400

    Weekly Update
    Adapts Mr. Patel's new demand on naming the file. Now it would also
    change the class name to the file name (without.java). You should not
    specify output file name so that these two new features would work
    properly. This project made my life a lot easier :p

--- src/cpp/interface.cpp
+++ src/cpp/interface.cpp
@@ -17,6 +17,7 @@ string version = "";
 string inputFile = "";
 string outputFile = "";
 string failReasons;
+string fileNameRAW;
 
 bool checkFileAccess(string i, string o) {
 	
@@ -25,16 +26,21 @@ bool checkFileAccess(string i, string o) {
 
 	if (!reader.is_open()) { failReasons += "\nProgram cannot find the origin file."; return 1; }
 	if (!writer.is_open()) { failReasons += "\nProgram cannot output to the target file."; return 1; }
+	
+	reader.close();
+	writer.close();
 	return 0;
 
 }
+
+
 
 bool readConfig() {
 	
 	reader.open("lsheader.ini");
 	
 	if (!reader.is_open()) {
-		printf(R"(Config "lsheader.ini" cannot be read, please check file access and make sure it exists.)");
+		//printf(R"(Config "lsheader.ini" cannot be read, please check file access and make sure it exists.)");
 		return 1;
 	}
 	
@@ -99,21 +105,24 @@ bool readConfig() {
 bool checkMissingParameters() {
 	bool returnVal = 0;
 	if (name == "")					{ failReasons += "Need to specify name.\n"; returnVal = 1;}
-	if (period == "")				{ failReasons += "Need to specify period.\n"; returnVal = 1;}
-	if (projectTitle == "")			{ failReasons += "Need to specify project title.\n"; returnVal = 1; }
-	if (projectDescription == "")	{ failReasons += "Need to specify project description.\n";returnVal = 1;}
-	if (inputFile == "")			{ failReasons += "Need to specify input file.\n";returnVal = 1;}
-	if (outputFile == "")			{ failReasons += "Need to specify output file.\n"; returnVal = 1; }
+	else if (period == "")				{ failReasons += "Need to specify period.\n"; returnVal = 1;}
+	else if (projectTitle == "")			{ failReasons += "Need to specify project title.\n"; returnVal = 1; }
+	else if (projectDescription == "")	{ failReasons += "Need to specify project description.\n";returnVal = 1;}
+	else if (inputFile == "")			{ failReasons += "Need to specify input file.\n";returnVal = 1;}
+
+
 	return returnVal;
 }
 
 int main(int argc, char*const argv[]) {
 	
-	if (readConfig()) return 1; // Read the config before running
+	string * documentCacheSpace = new string();
+
+	readConfig(); // Read the config before running
 
 	for (int i = 1; i < argc; ++i) {
-		if (containIgnoreCase(argv[i], "-i",1) || containIgnoreCase(argv[i], "--input")) { inputFile = argv[i + 1]; ++i; }
-		else if (containIgnoreCase(argv[i], "-o",1) || containIgnoreCase(argv[i], "--output")) { outputFile = argv[i + 1]; ++i; }
+		if (containIgnoreCase(argv[i], "-i", 1) || containIgnoreCase(argv[i], "--input")) { inputFile = argv[i + 1]; ++i; }
+		else if (containIgnoreCase(argv[i], "-o", 1) || containIgnoreCase(argv[i], "--output")) { outputFile = argv[i + 1]; ++i; }
 		else if (containIgnoreCase(argv[i], "-a", 1) || containIgnoreCase(argv[i], "--author")) { name = argv[i + 1]; ++i; }
 		else if (containIgnoreCase(argv[i], "-d", 1) || containIgnoreCase(argv[i], "--date")) { date = argv[i + 1]; ++i; }
 		else if (containIgnoreCase(argv[i], "-p", 1) || containIgnoreCase(argv[i], "--period")) { period = argv[i + 1]; ++i; }
@@ -121,9 +130,31 @@ int main(int argc, char*const argv[]) {
 		else if (containIgnoreCase(argv[i], "-pd", 1) || containIgnoreCase(argv[i], "--project-description")) { projectDescription = argv[i + 1]; ++i; }
 		else if (containIgnoreCase(argv[i], "-v", 1) || containIgnoreCase(argv[i], "--version")) { version = argv[i + 1]; ++i; }
 	}
-	
 	if (checkMissingParameters())					 { printf("%s\nFailed to execute.\n", failReasons.c_str()); return 1; } //Check missing parameters
-	else if (checkFileAccess(inputFile, outputFile)) { printf("%s\nFailed to execute.\n", failReasons.c_str()); return 1; }
+	//=================================
+	//Process class name and file name
+	//=================================
+	if (outputFile == "") { // Make a neatly-formatted name for Mr. Patel's assignments
+		for (int i = 0; i < inputFile.find_last_of("."); ++i) {
+			fileNameRAW += inputFile[i];
+		}
+		for (char nameBuffer : name) {
+
+			if (nameBuffer == ' ') continue;
+			else fileNameRAW += toupper(nameBuffer);
+
+		}
+		outputFile = fileNameRAW + ".java";
+	}
+	else {
+		for (int i = 0; i < outputFile.find_last_of("."); ++i) {
+			fileNameRAW += outputFile[i];
+		}
+	}
+	//====================
+	//Check File Access
+	//====================
+	if (checkFileAccess(inputFile, outputFile)) { printf("%s\nFailed to execute.\n", failReasons.c_str()); return 1; }
 
 
 	// Main Execution START
@@ -131,20 +162,44 @@ int main(int argc, char*const argv[]) {
 	if (date == "") date = getSystemDate();
 
 
+
+	reader.open(inputFile);
+
+
+	// =====================
+	// Cache File content
+	// =====================
+	while (getline(reader, readBuffer)) {
+		if (readBuffer.find("class") != string::npos) {
+			
+			*documentCacheSpace += "public class " + fileNameRAW;
+			if (readBuffer.find('{') != string::npos) *documentCacheSpace += " {";
+			*documentCacheSpace += "\n";
+
+			continue;
+		}
+		*documentCacheSpace += readBuffer + "\n";
+	}
+
+	reader.close(); //Close Read stream
+
+	writer.open(outputFile);
+
 	//====================
 	//Parse Header
 	//====================
+
 	writer << R"(//Name: )" << name << "\n"
 		<< R"(//Date: )" << date << "\n"
 		<< R"(//Period: )" << period << "\n"
 		<< R"(//Project Title: )" << projectTitle << "\n"
 		<< R"(//Project Description: )" << projectDescription << "\n"
 		<< R"(//Version: )" << version << "\n\n\n"; 
-
-	while (getline(reader, readBuffer)) writer << readBuffer << "\n";
-
+	
+	writer << *documentCacheSpace;
+	delete documentCacheSpace;
 	writer.close(); //Close Write stream
-	reader.close(); //Close Read stream
+	
 
 	//Main Execution End
 	
